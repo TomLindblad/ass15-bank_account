c682552670f5651c838f8a25d485d3e859f8564c
TomLindblad <168995337+TomLindblad@users.noreply.github.com>
2024-11-07 10:00:54 +0100

    added function that pushes the date of the transaction to an array

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const account = {
     accountName: "Tommy Batong",
-    balance: 500,
+    balance: 3000,
+    transactions:[],
     getBalance(){
         console.log(`Your current balance is ${this.balance}:-`);
     },
@@ -12,6 +13,7 @@ const account = {
         }
             else{
                 this.balance = this.balance + amount;
+                this.transactions.push({time:getTransactionTime(), amount:amount, balance:this.balance});
                 this.getBalance();
             }
     },  
@@ -23,6 +25,7 @@ const account = {
         }
             else if (amount <= this.balance){
                 this.balance = this.balance - amount;
+                this.transactions.push({time:getTransactionTime(), amount:-amount, balance:this.balance});
                 this.getBalance();
             }
                 else{
@@ -32,10 +35,32 @@ const account = {
     getAccountName(){
         console.log(`Account owner: ${this.accountName}`);
     },
+    getTransactions(){
+        console.log(this.transactions);
+    },
     accountError(errorType){
         console.log(`Error: ${errorType}`);
     },
     exitAccount(){
-        console.log("exit");
+        window.close();
     }
+}
+
+    //Gets the date and time of transaction.
+function getTransactionTime(){
+    let d = new Date();
+    year = d.getFullYear();
+    month = addZero(d.getMonth() +1);
+    date = addZero(d.getDate());
+    hour = addZero(d.getHours());
+    minute = addZero(d.getMinutes());
+    second = addZero(d.getSeconds());
+    return (`${year}${month}${date} ${hour}:${minute}:${second}`);
+}
+
+    //adds zero infront if number is single digit.
+function addZero(num){
+    num < 10 ? num = "0" + num : num = num;
+    return num;
+    
 }
