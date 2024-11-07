2a795ffc3a206a60ac15e034b1f8bcc7903db54a
TomLindblad <168995337+TomLindblad@users.noreply.github.com>
2024-11-07 09:40:42 +0100

    created the account object

---
+++ index.js
@@ -0,0 +1,22 @@
+const account = {
+    accountName: "Tommy Batong",
+    balance: 500,
+    getBalance(){
+
+    },
+    deposit(){
+
+    },  
+    withdrawal(){
+
+    },
+    getAccountName(){
+
+    },
+    accountError(){
+
+    },
+    exitAccount(){
+
+    }
+}
