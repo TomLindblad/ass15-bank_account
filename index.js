da4b8edaf7a24b2e6d19acf0ba8459badfb42692
TomLindblad <168995337+TomLindblad@users.noreply.github.com>
2024-11-07 09:47:59 +0100

    made the deposit & withdrawl-functions

--- index.js
+++ index.js
@@ -2,21 +2,30 @@ const account = {
     accountName: "Tommy Batong",
     balance: 500,
     getBalance(){
-
+        console.log(this.balance);
     },
     deposit(){
-
+        amount = parseFloat(prompt("How much money do you want to deposit?"));
+        this.balance = this.balance + amount;
+        console.log(`deposit ${amount}`);
+        console.log(`${this.balance}`);
     },  
     withdrawal(){
-
+        amount = parseFloat(prompt("How much money do you want to withdraw?"));
+        this.balance = this.balance - amount;
+        console.log(`withdraw ${amount}`);
+        console.log(`${this.balance}`);
     },
     getAccountName(){
-
+        console.log(this.accountName);
     },
     accountError(){
-
+        console.log("error");
     },
     exitAccount(){
-
+        console.log("exit");
     }
 }
+
+account.withdrawal();
+account.deposit();
